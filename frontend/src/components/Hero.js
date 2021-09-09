89cb855e3b8af46a17e751e626fab1d4847234b1
fluse9 <fluse9@gmail.com>
2021-09-09 00:13:03 -0500

    Created hero section and buttons

--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import Button from './Button.js';
+import { Button } from './Button.js';
 import '../App.css';
 import './Hero.css';
 
 function Hero() {
     return (
         <div className='hero-container'>
-
+            <video src='/videos/video-1.mp4' autoPlay loop muted />
+            <h1>LEARN TO CODE</h1>
+            <p>Start honing your skills in React, Node.js, & HTML/CSS today.</p>
+            <div className='hero-btns'>
+                <Button
+                    className='btns'
+                    buttonStyle='btn--outline'
+                    buttonSize='btn--large'
+                >
+                    GET STARTED
+                </Button>
+                <Button
+                    className='btns'
+                    buttonStyle='btn--primary'
+                    buttonSize='btn--large'
+                    onClick={console.log('test')}
+                >
+                    WATCH TRAILER <i className='far fa-play-circle' />
+                </Button>
+            </div>
         </div>
     );
 }
